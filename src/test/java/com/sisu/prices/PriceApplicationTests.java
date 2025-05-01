36912c91ab78bf46a860aaeecc5c67013b9dc9bf
Gino Angles <legendariioo3@gmail.com>
2025-05-01 12:53:51 +0200

    jira-001: initial commit with springboot.io blank project.

---
+++ src/test/java/com/sisu/prices/PriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisu.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
